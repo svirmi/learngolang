c611b4581bc6bdd4ebee0bd0ea5fe45536efe279
M S <mixa.ru.ru@gmail.com>
2020-11-15 13:00:24 +0200

    draw board

--- bouncing_ball/main.go
+++ bouncing_ball/main.go
@@ -1,17 +1,39 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 
 	const (
 		// board dimensions
 		width  = 50
 		height = 10
+
+		emptyCell = ' '
+		ballCell  = '⚽'
 	)
+
+	var cell rune
 
 	board := make([][]bool, width)
 	for row := range board {
 		board[row] = make([]bool, height)
 	}
 
+	board[0][0] = true
+
 	// draw the board
+	for y := range board[0] {
+		for x := range board {
+			cell = emptyCell
+			if board[x][y] {
+				cell = ballCell
+			}
+
+			fmt.Print(string(cell), " ")
+		}
+		fmt.Println()
+	}
 }
