60c11a3dfc55faa7607a15ab4f58d3529f6dd035
M S <mixa.ru.ru@gmail.com>
2020-11-17 09:29:18 +0200

    bouncing ball

--- bouncing_ball/main.go
+++ bouncing_ball/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/inancgumus/screen"
+	"time"
 )
 
 func main() {
@@ -11,34 +13,73 @@ func main() {
 		width  = 50
 		height = 10
 
+		maxFrames = 1200
+		speed     = time.Second / 20
+
 		emptyCell = ' '
 		ballCell  = '⚽'
+
+		// drawing buffer length
+		// *2 for extra spaces
+		// +1 for newlines
+		bufLen = (width*2 + 1) * height
 	)
 
 	var (
 		px, py int
+		vx, vy = 1, 1 // velocity
 		cell   rune
 	)
 
+	// create the board
 	board := make([][]bool, width)
-	for row := range board {
-		board[row] = make([]bool, height)
+	for column := range board {
+		board[column] = make([]bool, height)
 	}
 
-	buf := make([]rune, 0, width*height)
+	buf := make([]rune, 0, bufLen)
 
-	board[px][py] = true
+	screen.Clear()
 
-	// draw the board
-	for y := range board[0] {
-		for x := range board {
-			cell = emptyCell
-			if board[x][y] {
-				cell = ballCell
-			}
-			buf = append(buf, cell, ' ')
+	for i := 0; i < maxFrames; i++ {
+
+		px += vx
+		py += vy
+
+		if px <= 0 || px >= width-1 { // ball hits left or right wall
+			vx *= -1
 		}
-		buf = append(buf, '\n')
+
+		if py <= 0 || py >= height-1 { // ball hits left or right wall
+			vy *= -1
+		}
+
+		for y := range board[0] {
+			for x := range board {
+				board[x][y] = false
+			}
+		}
+
+		board[px][py] = true
+
+		// rewind the buffer (allow appending from the beginning)
+		buf = buf[:0]
+
+		// draw the board into the buffer
+		for y := range board[0] {
+			for x := range board {
+				cell = emptyCell
+				if board[x][y] {
+					cell = ballCell
+				}
+				buf = append(buf, cell, ' ')
+			}
+			buf = append(buf, '\n')
+		}
+
+		screen.MoveTopLeft()
+		fmt.Println(string(buf))
+
+		time.Sleep(speed)
 	}
-	fmt.Println(string(buf))
 }
