b8b19e28f5023e5353bb2f3a3e9c109d0f608d09
M S <mixa.ru.ru@gmail.com>
2021-02-13 19:23:48 +0200

    working ssh

---
+++ network/ssh/Readme.md
@@ -0,0 +1,3 @@
+https://stackoverflow.com/questions/49761966/print-the-console-log-to-the-screen-of-a-webpage-continuously-using-go-routines
+streaming to browser
+
--- network/ssh/main.go
+++ network/ssh/main.go
@@ -1,66 +1,88 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"net"
+	"log"
+	"os"
 
 	"golang.org/x/crypto/ssh"
 )
 
 func main() {
-	user := "lopoi-popoi"
-	addr := "lopoi-popoi.myjino.ru"
-	password := "Zbf87587f3dd13e4"
-	command := "ls -la"
 
-	var output string
+	username := ""
+	password := ""
+	hostname := ""
+	port := "22"
 
-	output, err := remoteRun(user, addr, password, command)
-
-	if err != nil {
-		fmt.Println("err")
-		return
-	}
-
-	fmt.Println(output)
-}
-
-// https://stackoverflow.com/questions/37679939/how-do-i-execute-a-command-on-a-remote-machine-in-a-golang-cli
-// e.g. output, err := remoteRun("root", "MY_IP", "PRIVATE_KEY", "ls")
-func remoteRun(user string, addr string, password string, cmd string) (string, error) {
-	// privateKey could be read from a file, or retrieved from another storage
-	// source, such as the Secret Service / GNOME Keyring
-
-	// Authentication
+	// SSH client config
 	config := &ssh.ClientConfig{
-		User: user,
-		// Auth: []ssh.AuthMethod{
-		// 	ssh.PublicKeys(key),
-		// },
-		//alternatively, you could use a password
+		User: username,
 		Auth: []ssh.AuthMethod{
 			ssh.Password(password),
 		},
+		// Non-production only
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
-	// Connect
-	client, err := ssh.Dial("tcp", net.JoinHostPort(addr, "22"), config)
-	if err != nil {
-		return "", err
-	}
-	// Create a session. It is one session per command.
-	session, err := client.NewSession()
-	if err != nil {
-		return "", err
-	}
-	defer session.Close()
-	var b bytes.Buffer  // import "bytes"
-	session.Stdout = &b // get output
-	// you can also pass what gets input to the stdin, allowing you to pipe
-	// content from client to server
-	//      session.Stdin = bytes.NewBufferString("My input")
 
-	// Finally, run the command
-	err = session.Run(cmd)
-	return b.String(), err
+	// Connect to host
+	client, err := ssh.Dial("tcp", hostname+":"+port, config)
+	if err != nil {
+		fmt.Println("connection error")
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// Create sesssion
+	sess, err := client.NewSession()
+	if err != nil {
+		fmt.Println("session error")
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer sess.Close()
+
+	// StdinPipe for commands
+	stdin, err := sess.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Uncomment to store output in variable
+	//var b bytes.Buffer
+	//sess.Stdout = &amp;b
+	//sess.Stderr = &amp;b
+
+	// Enable system stdout
+	// Comment these if you uncomment to store in variable
+	sess.Stdout = os.Stdout
+	sess.Stderr = os.Stderr
+
+	// Start remote shell
+	err = sess.Shell()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// send the commands
+	commands := []string{
+		"pwd",
+		"whoami",
+		"(curl -s wget.racing/nench.sh | bash) 2>&1 | tee nench.log",
+		"exit",
+	}
+	for _, cmd := range commands {
+		_, err = fmt.Fprintf(stdin, "%s\n", cmd)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Wait for sess to finish
+	err = sess.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Uncomment to store in variable
+	//fmt.Println(b.String())
 }
